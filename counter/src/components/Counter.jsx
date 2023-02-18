fcfa19ff2e5c77c99499a7615ffacc9feea56ad6
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2023-02-18 10:59:52 +0200

    init

---
+++ counter/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  const onIncrement = () => setCount(count + 1);
+  const onDecrement = () => setCount(count - 1);
+  return (
+    <div>
+      <h3>Counter App</h3>
+      <p>
+        Current count: <strong>{count}</strong>
+      </p>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  );
+};
